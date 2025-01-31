767ea57f3cd0140b04d17024949265b1bafc3c7b
aarslantas <a_arslantas69@hotmail.com>
2025-01-31 20:19:52 +0300

    fix(List): correct styling issues and improve structure for rendering list items

--- components/MealDetail/List.js
+++ components/MealDetail/List.js
@@ -1,21 +1,25 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 function List({ data }) {
-  return data.map((dataPoint) => (
-    <View key={dataPoint} style={styles.listItem}>
-      <Text style={styles.itemText}>{dataPoint}</Text>
-    </View>
-  ));
+  return (
+    <>
+      {data.map((dataPoint) => (
+        <View key={dataPoint} style={styles.listItem1}>
+          <Text style={styles.itemText}>{dataPoint}</Text>
+        </View>
+      ))}
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
-  listItem: {
+  listItem1: {
     borderRadious: 6,
     paddingHorizontal: 8,
     paddingVertical: 4,
     marginVertical: 8,
     marginHorizontal: 12,
-    backgorundColor: "#eb2b497",
+    backgroundColor: "#e2b497",
   },
   itemText: {
     color: "#351401",

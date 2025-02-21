71eeaf28f9577e6268b3b1741aed69e4b38ea533
aarslantas <a_arslantas69@hotmail.com>
2025-02-21 23:10:20 +0300

    feat(MealDetailScreen): integrate Redux for favorite meal management

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -13,11 +13,17 @@ import Subtitle from "../components/MealDetail/SubTitle";
 import List from "../components/MealDetail/List";
 import IconButton from "../components/IconButton";
 import FavoritesContext from "../store/context/favorites-context";
+import { useDispatch, useSelector } from "react-redux";
+import { addFavorite, removeFavorite } from "../store/redux/favorites";
 
 function MealDetailScreen({ route, navigation }) {
   const mealId = route.params.mealId;
 
-  const favoriteMealsCtx = useContext(FavoritesContext);
+  const favoriteMealIds = useSelector((state) => state.favoriteMels.ids);
+
+  const dispatch = useDispatch();
+
+  // const favoriteMealsCtx = useContext(FavoritesContext);
 
   const meal = MEALS.find((mealItem) => {
     return mealItem.id === mealId;
@@ -25,9 +31,11 @@ function MealDetailScreen({ route, navigation }) {
 
   function handleChangeFavoriteStatus() {
     if (mealIsFavorite) {
-      favoriteMealsCtx.removeFavorite(mealId);
+      // favoriteMealsCtx.removeFavorite(mealId);
+      dispatch(removeFavorite({ id: meal }));
     } else {
-      favoriteMealsCtx.addFavorite(mealId);
+      dispatch(addFavorite({ id: meal }));
+      // favoriteMealsCtx.addFavorite(mealId);
     }
   }
 

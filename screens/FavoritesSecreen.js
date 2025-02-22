9c707e83b7f33770dc305f1bb205bb391d0adf89
aarslantas <a_arslantas69@hotmail.com>
2025-02-22 15:47:09 +0300

    feat(FavoritesScreen): replace context with Redux for favorite meals management

--- screens/FavoritesSecreen.js
+++ screens/FavoritesSecreen.js
@@ -3,12 +3,14 @@ import MealsList from "../components/MealsList/MealsList";
 import FavoritesContext from "../store/context/favorites-context";
 import { MEALS } from "../data/dummy-data";
 import { StyleSheet, Text, View } from "react-native";
+import { useSelector } from "react-redux";
 
 function FavoritesScreen() {
-  const favoriteMealsCtx = useContext(FavoritesContext);
+  // const favoriteMealsCtx = useContext(FavoritesContext);
+  const favoriteMealIds = useSelector((state) => state.favoriteMealIds);
 
   const favoriteMeals = MEALS.filter((meal) => {
-    return favoriteMealsCtx.ids.includes(meal.id);
+    return favoriteMealIds.includes(meal.id);
   });
 
   if (favoriteMeals.length === 0 || !favoriteMeals) {
